2ea0ea06754a08dfc0251d8a77886612f4189663
switzere <switzere@uoguelph.ca>
2020-12-10 13:21:02 -0500

    made is so importData is useless

--- dashboard/app.R
+++ dashboard/app.R
@@ -367,4 +367,20 @@ server <- function(input, output, session) {
   })
 }
 
+
+#CHN simple percent.csv
+g1Data <- read.csv(file = './CHN simple percent.csv', header=TRUE, fileEncoding="UTF-8-BOM")
+
+#unemploymentSimple.csv
+g2Data <- read.csv(file = './unemploymentSimple.csv', header=TRUE, fileEncoding="UTF-8-BOM")
+
+#TotalCovidCasesProv.csv
+g2.1Data <- read.csv(file = './TotalCovidCasesProv.csv', header=TRUE, fileEncoding="UTF-8-BOM")
+
+#allInformation.csv
+g3Data <- read.csv(file = './allInformation.csv', header=TRUE, fileEncoding="UTF-8-BOM")
+
+#Housing Units created.csv
+q4Data <- read.csv(file = './Housing Units created.csv', header=TRUE, fileEncoding="UTF-8-BOM")
+
 shinyApp(ui, server)
